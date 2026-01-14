ee0cb20f856ba8cbd02988887637fb023c048d69
vectris-dev <steve@vectr.is>
2026-01-14 14:45:45 +1100

    Test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // Transpile the SDK package to ensure proper handling
-  transpilePackages: ['@mascotbot-sdk/react'],
-  experimental: {
-    // Prevent optimization issues with pre-minified/obfuscated packages
-    optimizePackageImports: [],
-  },
+  swcMinify: false,
 };
 
 module.exports = nextConfig;
